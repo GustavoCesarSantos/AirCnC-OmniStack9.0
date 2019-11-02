8d4bbad53c01e6f09caeb8e6038dc0461cbe3e11
GustavoCesarSantos <gustavocs789@gmail.com>
2019-11-02 20:27:56 -0300

    Criado Background,logo e form e iniciado processo de comunicacao com api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import api from './services/api'
+
+import logo from './assets/logo.png';
+
 function App() {
+  function handleSubmit(event){
+    event.preventDefault();
+    
+    
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
